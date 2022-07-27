676f666963815be44bd1aef26de77e7bda622cbd
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2022-07-27 23:46:31 +1000

    &Window menu utilities

--- MainFrm.cs
+++ MainFrm.cs
@@ -39,5 +39,27 @@ namespace msnld_client
             settingsFrm.StartPosition = FormStartPosition.CenterParent;
             settingsFrm.ShowDialog();
         }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null) {
+                this.ActiveMdiChild.Close();
+            }
+        }
     }
 }
