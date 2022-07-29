e500ded21de2ade87444df40d2ea6f671debc529
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2022-07-30 04:25:14 +1000

    New Settings Store

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -22,5 +22,17 @@ namespace msnld_client.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string GuestNicknames {
+            get {
+                return ((string)(this["GuestNicknames"]));
+            }
+            set {
+                this["GuestNicknames"] = value;
+            }
+        }
     }
 }
