7fab758d12f75a67931accfbc6c7f46772f8b8cf
JD Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2023-05-28 16:03:46 +1000

    Add MainForm

---
+++ MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace MSNLD_Chat_Client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TreeNode treeNode1 = new TreeNode("Whispers (Private Messages)");
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            windowToolStripMenuItem = new ToolStripMenuItem();
+            tileHorizontalToolStripMenuItem = new ToolStripMenuItem();
+            tileVerticalToolStripMenuItem = new ToolStripMenuItem();
+            cascadeToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripSeparator();
+            treeView1 = new TreeView();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, windowToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.MdiWindowListItem = windowToolStripMenuItem;
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, toolStripMenuItem1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.Size = new Size(98, 22);
+            newToolStripMenuItem.Text = "&New";
+            newToolStripMenuItem.Click += newToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(95, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(98, 22);
+            exitToolStripMenuItem.Text = "E&xit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // windowToolStripMenuItem
+            // 
+            windowToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tileHorizontalToolStripMenuItem, tileVerticalToolStripMenuItem, cascadeToolStripMenuItem, toolStripMenuItem2, closeToolStripMenuItem, toolStripMenuItem3 });
+            windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            windowToolStripMenuItem.Size = new Size(63, 20);
+            windowToolStripMenuItem.Text = "&Window";
+            // 
+            // tileHorizontalToolStripMenuItem
+            // 
+            tileHorizontalToolStripMenuItem.Name = "tileHorizontalToolStripMenuItem";
+            tileHorizontalToolStripMenuItem.Size = new Size(150, 22);
+            tileHorizontalToolStripMenuItem.Text = "Tile Hori&zontal";
+            tileHorizontalToolStripMenuItem.Click += tileHorizontalToolStripMenuItem_Click;
+            // 
+            // tileVerticalToolStripMenuItem
+            // 
+            tileVerticalToolStripMenuItem.Name = "tileVerticalToolStripMenuItem";
+            tileVerticalToolStripMenuItem.Size = new Size(150, 22);
+            tileVerticalToolStripMenuItem.Text = "Tile &Vertical";
+            tileVerticalToolStripMenuItem.Click += tileVerticalToolStripMenuItem_Click;
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            cascadeToolStripMenuItem.Size = new Size(150, 22);
+            cascadeToolStripMenuItem.Text = "&Cascade";
+            cascadeToolStripMenuItem.Click += cascadeToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(147, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(150, 22);
+            closeToolStripMenuItem.Text = "Clo&se";
+            closeToolStripMenuItem.Click += closeToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(147, 6);
+            // 
+            // treeView1
+            // 
+            treeView1.BorderStyle = BorderStyle.FixedSingle;
+            treeView1.Dock = DockStyle.Left;
+            treeView1.FullRowSelect = true;
+            treeView1.Location = new Point(0, 24);
+            treeView1.Name = "treeView1";
+            treeNode1.Name = "whisperNode";
+            treeNode1.Text = "Whispers (Private Messages)";
+            treeView1.Nodes.AddRange(new TreeNode[] { treeNode1 });
+            treeView1.Size = new Size(208, 426);
+            treeView1.TabIndex = 5;
+            treeView1.DoubleClick += treeView1_DoubleClick;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(treeView1);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            Text = "MSNLD Chat Client";
+            Load += MainForm_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem windowToolStripMenuItem;
+        private ToolStripMenuItem tileHorizontalToolStripMenuItem;
+        private ToolStripMenuItem tileVerticalToolStripMenuItem;
+        private ToolStripMenuItem cascadeToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem closeToolStripMenuItem1;
+        private ToolStripSeparator toolStripMenuItem3;
+        private TreeView treeView1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,144 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using static PInvoke.User32;
+
+namespace MSNLD_Chat_Client
+{
+    public partial class MainForm : Form
+    {
+        IntPtr mdiFrame;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            EventHandler? handler = null; // Single use handler.
+            handler = (object? s, EventArgs e) =>
+            {
+                form.WindowState = FormWindowState.Maximized; // Maximise form.
+                form.Shown -= handler; // Remove handler (self).
+            };
+            form.Shown += handler; // Add handler.
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void tileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ActiveMdiChild != null) ActiveMdiChild.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var whisperManager = new WhisperManager();
+            whisperManager.WhisperWindowOpened += WhisperManager_WhisperWindowOpened;
+            whisperManager.WhisperWindowClosed += WhisperManager_WhisperWindowClosed;
+            newToolStripMenuItem_Click(sender, e);
+            if (ActiveMdiChild != null) ActiveMdiChild.WindowState = FormWindowState.Maximized;
+
+            // We need to get (and store) the actual MDI frame, rather than the form.
+            // The easiest way I can think of to do this, is create a MDIChild form and look at it's parent.
+            var tmpForm = new Form();
+            tmpForm.ControlBox = false;
+            tmpForm.ClientSize = new Size(0, 0);
+            // When we set MdiParent, the form will be shown (briefly) - It should not be visible.
+            tmpForm.MdiParent = this;
+            mdiFrame = GetAncestor(tmpForm.Handle, GetAncestorFlags.GA_PARENT);
+            tmpForm.Close(); // We don't need it anymore. GC will clean it up.
+        }
+
+        private void WhisperManager_WhisperWindowClosed(object? sender, WhisperEventArgs e)
+        {
+            treeView1.Nodes[0].Nodes.RemoveByKey(e.hwnd.ToString());
+        }
+
+        private void WhisperManager_WhisperWindowOpened(object? sender, WhisperEventArgs e)
+        {
+            if (treeView1.ImageList == null)
+            {
+                var icon = Misc.GetAppIcon(e.hwnd);
+                var imageList = new ImageList();
+                if (icon != null)
+                {
+                    imageList.Images.Add(new Bitmap(16, 16)); // Blank icon
+                    imageList.Images.Add(icon); // Whisper Window icon
+                }
+                treeView1.ImageList = imageList;
+            }
+            //this.Icon = Misc.GetAppIcon(e.hwnd); // Steal Whisper Window icon - Just for fun.
+            TreeNode subNode = treeView1.Nodes[0].Nodes.Add(e.hwnd.ToString(), e.title, 1);
+            subNode.SelectedImageIndex = 1; // Don't change image when selected.
+
+            // Set window flags
+            /*var cStyle = (SetWindowLongFlags)GetWindowLong(e.hwnd, WindowLongIndexFlags.GWL_STYLE);
+              cStyle &= ~SetWindowLongFlags.WS_EX_NOPARENTNOTIFY;
+              cStyle &= ~SetWindowLongFlags.WS_EX_TOOLWINDOW;
+              cStyle &= ~SetWindowLongFlags.WS_EX_STATICEDGE;
+              cStyle &= ~SetWindowLongFlags.WS_EX_APPWINDOW;
+              cStyle &= ~SetWindowLongFlags.WS_POPUP;
+              cStyle &= ~SetWindowLongFlags.WS_CHILD;
+              cStyle &= ~SetWindowLongFlags.WS_CAPTION;
+              SetWindowLong(e.hwnd, WindowLongIndexFlags.GWL_STYLE, cStyle);*/
+
+            // Create parent form
+            /*var whisperWindow = new Form();
+              whisperWindow.MdiParent = this;
+              whisperWindow.Text = utf8WindowText;
+
+              GetWindowRect(lastWindow, out var rect);
+              // Set default client size
+              whisperWindow.ClientSize = new Size(rect.right - rect.left, rect.bottom - rect.top);
+              whisperWindow.Show();
+              // In case we are maximised, we need to have the Whisper Window the same size.
+              MoveWindow(lastWindow, 0, 0, whisperWindow.ClientSize.Width + 2, whisperWindow.ClientSize.Height + 2, true);
+              var thisWindow = lastWindow; // lastWindow will be garbage collected
+              whisperWindow.Resize += (s, e) =>
+              {
+                  MoveWindow(thisWindow, 0, 0, whisperWindow.ClientSize.Width + 2, whisperWindow.ClientSize.Height + 2, true);
+              };*/
+            SetParent(e.hwnd, mdiFrame);
+        }
+
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode == null) return;
+            IntPtr hwnd;
+            try
+            {
+                hwnd = (IntPtr)Convert.ToInt64(treeView1.SelectedNode.Name);
+                SetForegroundWindow(hwnd);
+            }
+            catch (Exception)
+            {
+                // One of two things happened
+                // - (Likely) Key isn't a valid Int64, so it can't be converted to IntPtr
+                // - (Unlikely) Key is a valid Int64, but it's not a valid hwnd.
+                return;
+            }
+        }
+    }
+}
